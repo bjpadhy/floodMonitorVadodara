ceac924acd39b0c9825bd8c83764a12407c8d14d
Biswaranjan Padhy <padhybiswaranjan@live.com>
2020-08-15 23:15:15 +0530

    Updated schema model

--- models/levels.js
+++ models/levels.js
@@ -1,19 +1,15 @@
 const mongoose = require('mongoose');
 
-const vishwamitriLevelSchema = new mongoose.Schema({
+const levelSchema = {
   id: String,
   server_time: String,
   date: String,
   time: String,
   level: Number
-}, { collection: 'vishwamitri_levels' });
+}
 
-const ajwaLevelSchema = new mongoose.Schema({
-  id: String,
-  server_time: String,
-  date: String,
-  time: String,
-  level: Number
-}, { collection: 'ajwa_levels' });
+const vishwamitriLevelSchema = new mongoose.Schema(levelSchema, { collection: 'vishwamitri_levels' });
 
-module.exports = {vishwamitriLevel: mongoose.model('vishwamitriLevels', vishwamitriLevelSchema), ajwaLevel: mongoose.model('ajwaLevelSchema', ajwaLevelSchema)}
+const ajwaLevelSchema = new mongoose.Schema(levelSchema, { collection: 'ajwa_levels' });
+
+module.exports = { vishwamitriLevel: mongoose.model('vishwamitriLevels', vishwamitriLevelSchema), ajwaLevel: mongoose.model('ajwaLevelSchema', ajwaLevelSchema) }
